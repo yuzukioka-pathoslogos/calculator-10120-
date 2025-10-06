d1eff917ebab7b20d2fc30922b4a8bbd64923734
yuzukioka-pathoslogos <yuzuki.oka@pathoslogos.co.jp>
2025-10-06 11:45:35 +0900

    連続計算の処理修正、負の数の表示桁数、負の数の平方根の処理等の修正

--- calculator-app/src/app/app.component.ts
+++ calculator-app/src/app/app.component.ts
@@ -28,17 +28,28 @@ export class AppComponent implements AfterViewInit {
     const squareRoot = document.querySelector('.button-sqrt') as HTMLButtonElement;
     const persent = document.querySelector('.button-percent') as HTMLButtonElement;
 
-    //基本的に式の左辺がstack[0]、右辺がstack[1]
-    const stack: string[] = ['', '0'];
+    //基本的に式の左辺がstack[0]、右辺がstack[1]、連続計算対応のための一時保管場所としてstack[3]を用意
+    const stack: string[] = ['', '0', ''];
+    let afterCalc: boolean = false;
     display.textContent = stack[1];
     // 最後に押した演算子を格納
-    let operator: string | null = null;
+    let operator: string = '';
     //計算を行う関数
     const calc = function(){
       switch(operator){
         case '+':
-          const sum = (Number(stack[0]) + Number(stack[1])).toString();
-          stack[1] = sum.slice(0, 10);
+          stack[3] = stack[1];
+          //小数点以下13桁まで丸めることで浮動小数点誤差を防ぐ
+          const sum = (Number(stack[0]) + Number(stack[1])).toPrecision(13).toString();
+          if(sum.includes('-')){
+            stack[1] = sum.slice(0, 11);
+          }else{
+            stack[1] = sum.slice(0, 10);
+          }
+          //連続計算対応のためにstack[0]にcalc前のstack[1]を代入
+          if(afterCalc === false){
+            stack[0] = stack[3];
+          }
           //小数点以下の末尾の0を削除
           for(let i = 0; i < 10; i++){
             if(stack[1].includes('.') && stack[1].endsWith('0')){
@@ -56,10 +67,22 @@ export class AppComponent implements AfterViewInit {
             stack[1] = '0';
             return;
           }
+          afterCalc = true;
           break;
         case '-':
-          const diff = (Number(stack[0]) - Number(stack[1])).toString();
-          stack[1] = diff.slice(0, 10);
+          //連続計算対応
+          if(afterCalc === true){
+            stack[0] = stack[1];
+            stack[1] = stack[3];
+          }else{
+            stack[3] = stack[1];
+          }
+          const diff = (Number(stack[0]) - Number(stack[1])).toPrecision(13).toString();
+          if(diff.includes('-')){
+            stack[1] = diff.slice(0, 11);
+          }else{
+            stack[1] = diff.slice(0, 10);
+          }
           for(let i = 0; i < 10; i++){
             if(stack[1].includes('.') && stack[1].endsWith('0')){
               stack[1] = stack[1].slice(0, -1);
@@ -76,10 +99,15 @@ export class AppComponent implements AfterViewInit {
             stack[1] = '0';
             return;
           }
+          afterCalc = true;
           break;
         case '*':
-          const prod = (Number(stack[0]) * Number(stack[1])).toString();
-          stack[1] = prod.slice(0, 10);
+          const prod = (Number(stack[0]) * Number(stack[1])).toPrecision(13).toString();
+          if(prod.includes('-')){
+            stack[1] = prod.slice(0, 11);
+          }else{
+            stack[1] = prod.slice(0, 10);
+          }
           for(let i = 0; i < 10; i++){
             if(stack[1].includes('.') && stack[1].endsWith('0')){
               stack[1] = stack[1].slice(0, -1);
@@ -96,16 +124,28 @@ export class AppComponent implements AfterViewInit {
             stack[1] = '0';
             return;
           }
+          afterCalc = true;
           break;
         case '/':
+          //連続計算対応
+          if(afterCalc === true){
+            stack[0] = stack[1];
+            stack[1] = stack[3];
+          }else{
+            stack[3] = stack[1];
+          }
           if(Number(stack[1]) === 0){
             display.textContent = 'error';
             stack[0] = '';
             stack[1] = '0';
             return;
           }
-          const quot = (Number(stack[0]) / Number(stack[1])).toString();
-          stack[1] = quot.slice(0, 10);
+          const quot = (Number(stack[0]) / Number(stack[1])).toPrecision(13).toString();
+          if(quot.includes('-')){
+            stack[1] = quot.slice(0, 11);
+          }else{
+            stack[1] = quot.slice(0, 10);
+          }
           for(let i = 0; i < 10; i++){
             if(stack[1].includes('.') && stack[1].endsWith('0')){
               stack[1] = stack[1].slice(0, -1);
@@ -121,22 +161,26 @@ export class AppComponent implements AfterViewInit {
             stack[0] = '';
             stack[1] = '0';
             return;
-          }
+          } 
+          afterCalc = true;
           break;
         default:
           return;
       }
     }
 
-    
-
     //数字をクリックした時に表示される数字を追加
     buttons.forEach((btn: HTMLButtonElement) => {
       btn.addEventListener('click', () => {
-
         if(operator !== null){          //演算子が入力されている場合
           if(stack[1] === '0'){         //ディスプレイが0の時は消してから数字を入力
             stack[1] = '';
+          }
+          if(afterCalc === true){
+            stack[0] = '';
+            stack[1] = '';
+            operator = '';
+            afterCalc = false;
           }
           if(stack[1] !=null && stack[1].length === 10){
             return;
@@ -146,6 +190,11 @@ export class AppComponent implements AfterViewInit {
         }else{                          //演算子が入力されていない場合
           if(stack[1] === '0'){
             stack[1] = '';
+          }
+          if(afterCalc === true){
+            stack[0] = '';
+            stack[1] = '';
+            afterCalc = false;
           }
           if(stack[1] !=null && stack[1].length === 10){
             return;
@@ -172,16 +221,26 @@ export class AppComponent implements AfterViewInit {
       }
     });
 
-    //演算子をクリックした時に変数operatorに演算子を代入
+    //演算子の処理
     operators.forEach((op: HTMLButtonElement) => {
       op.addEventListener('click', () => {
         if(stack[0] === ''){
           operator = op.value as string;
           stack[0] = stack[1];
           stack[1] = '';
-        }else{
-          calc();
+        }else if(stack[0] !== '' && stack[1] === ''){
           operator = op.value as string;
+        }else if(stack[0] !== '' && stack[1] !== '' && afterCalc === false){
+          calc();
+          stack[0] = stack[1];
+          stack[1] = '';
+          afterCalc = false;
+          operator = op.value as string;
+        }else if(stack[0] !== '' && stack[1] !== '' && afterCalc === true){
+          operator = op.value as string;
+          stack[0] = stack[1];
+          stack[1] = '';
+          afterCalc = false;
         }
       });
     });
@@ -201,7 +260,7 @@ export class AppComponent implements AfterViewInit {
     clear.addEventListener('click', () => {
       stack[0] = '';
       stack[1] = '0';
-      operator = null;
+      operator = '';
       display.textContent = stack[1];
     });
 
@@ -213,9 +272,16 @@ export class AppComponent implements AfterViewInit {
 
     //平方根をクリックした時に平方根を入力
     squareRoot.addEventListener('click', () => {
+      if(stack[1].includes('-')){
+        display.textContent = 'error';
+        stack[0] = '';
+        stack[1] = '0';
+        return;
+      }
       const ans = Math.sqrt(Number(stack[1])).toString();
       stack[1] = ans.slice(0, 10);
       display.textContent = stack[1];
+      afterCalc = true;
     });
 
     //パーセントをクリックした時にパーセントを入力
